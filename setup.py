bdc03bf0d1f0d56385945c5d9881672f8659c07b
Andre Almeida <a.almeida@unsw.edu.au>
2018-01-08 20:27:10 +1100

    basic setuptools setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='pypevoc',
+      version='0.1',
+      description='Pure python sound analysis tools',
+      url='http://github.com/goiosunw/pypevoc',
+      author='Andre Goios',
+      author_email='a.almeida@unsw.edu.au',
+      license='GPL v3',
+      packages=['pypevoc'],
+      zip_safe=False)

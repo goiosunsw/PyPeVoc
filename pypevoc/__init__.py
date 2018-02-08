dbbc692e7cd704d40082e3f9db0bd946a17fdabc
Andre Almeida <a.almeida@unsw.edu.au>
2018-02-08 18:52:55 +1100

    module shortcut additions

--- pypevoc/__init__.py
+++ pypevoc/__init__.py
@@ -1,5 +1,5 @@
 from .PVAnalysis import PV, PVHarmonic, SinSum
-from .Periodicity import Periodicity, period_marks_corr, period_marks_peak
+from .Periodicity import PeriodSeries, period_marks_corr, period_marks_peak, period_marks_amdf
 from . import SoundUtils
-import TransferFunctions
+from . import TransferFunctions
 

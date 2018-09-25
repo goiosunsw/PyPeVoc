6e95e5fa446c5e456d947c0d5bf3eb0f5544ee15
Andre Goios <a.almeida@unsw.edu.au>
2018-09-25 14:21:58 +1000

    glottal tests

--- tests/test_glottal.py
+++ tests/test_glottal.py
@@ -13,6 +13,26 @@ class testPaddedFilter(unittest.TestCase):
         y = glx.apply_fir(filter)
         self.assertEqual(len(x),len(y))
 
+    def test_output_same_after_padding_as_input(self):
+        filter = np.array([-1,1])
+        n_pad_before = 8
+        n_pad_after = 8
+        n_signal = 16
+        x = np.zeros(n_signal)
+        glx = gl.PaddedFilter(x,n_before=n_pad_before,n_after=n_pad_after)
+        y = glx.apply_fir(filter)
+        self.assertEqual(len(x),len(y))
+
+    def test_output_same_as_input(self):
+        filter = np.array([1])
+        n_pad_before = 8
+        n_signal = 16
+        x = np.zeros(n_signal)
+        glx = gl.PaddedFilter(x,n_before=n_pad_before)
+        y = glx.apply_fir(filter)
+        for xx, yy in zip(x, y):
+            self.assertEqual(xx, yy)
+
     def test_private_buffer_1d(self):
         glx = gl.PaddedFilter(np.zeros(10),n_before=8)
         self.assertEqual(len(glx._padded_input.shape),1)
